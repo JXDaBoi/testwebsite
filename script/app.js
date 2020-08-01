345ce40ae8fc75ead5c03ac000afbb40167d45d0
JXDaBoi <jingxiangdaboi@gmail.com>
2020-08-01 15:57:52 +0800

    Migrated some of the styling to tailwind

--- script/app.js
+++ script/app.js
@@ -3,13 +3,21 @@ const ldb = document.querySelectorAll('.ldButton');
 ldb.forEach(function(ldbutton) {
   ldbutton.addEventListener('click', function(e) {
     if (e.target.name == "light") {
-      document.querySelector(".title").style.backgroundColor = "salmon";
+      document.querySelector(".title").classList.remove("bg-green-400");
+      document.querySelector(".title").classList.add("bg-white");
       document.body.style.backgroundColor = "white";
-      document.querySelector(".nav-bar").style.backgroundColor = "cyan";
+      document.querySelector(".nav-bar").classList.remove("bg-purple-700");
+      document.querySelector(".nav-bar").classList.add("bg-purple-300");
+      document.querySelector(".nav-bar").classList.remove("text-white");
+      document.querySelector(".nav-bar").classList.add("text-black");
     } else if (e.target.name == "dark") {
-      document.querySelector(".title").style.backgroundColor = "rgb(120, 233, 58)";
+      document.querySelector(".title").classList.remove("bg-white");
+      document.querySelector(".title").classList.add("bg-green-400");
       document.body.style.backgroundColor = "rgb(61, 61, 207)";
-      document.querySelector(".nav-bar").style.backgroundColor = "rgb(58, 237, 237)";
+      document.querySelector(".nav-bar").classList.add("bg-purple-700");
+      document.querySelector(".nav-bar").classList.remove("bg-purple-300");
+      document.querySelector(".nav-bar").classList.add("text-white");
+      document.querySelector(".nav-bar").classList.remove("text-black");
     }
   })
 })
